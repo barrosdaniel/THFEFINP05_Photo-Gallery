2eb3aad8f18313650883423fbf4d85e12fe3b5d8
barros.daniel@outlook.com <barros.daniel@outlook.com>
2019-11-23 03:21:24 +0800

    feature: Add search image filter functionality

--- js/app.js
+++ js/app.js
@@ -1,5 +1,35 @@
-// Lightbox jQuery Plugin Options
+/* ============================================================ 
+Lightbox jQuery Plugin Options
+============================================================ */
 lightbox.option({
   'showImageNumberLabel': false,
   'wrapAround': true
 })
+
+/* ============================================================ 
+Image Caption Filter
+============================================================ */
+// UI elements
+const filter = document.querySelector('.header__search');
+
+// Event listeners
+filter.addEventListener('keyup', filterImages);
+
+// App logic
+function filterImages(e) {
+  // Get value of filter field
+  const text = e.target.value.toLowerCase();
+
+  // Get all images
+  const imageLinks = document.querySelectorAll('.gallery__image-link');
+
+  // Render only images that match search items
+  imageLinks.forEach((imageLink) => {
+    const caption = imageLink.getAttribute('data-title');
+    if (caption.toLowerCase().indexOf(text) != -1) {
+      imageLink.style.display = 'block';
+    } else {
+      imageLink.style.display = 'none';
+    }
+  })
+}
